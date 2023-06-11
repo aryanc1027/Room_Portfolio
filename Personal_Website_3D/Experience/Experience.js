0c74b71672230ed4941eb8ea913cde0f06149168
Aryan C <aryanc1027@gmail.com>
2023-06-11 16:53:43 -0400

    Beginning Code, Starts the "Singleton pattern"

---
+++ Personal_Website_3D/Experience/Experience.js
@@ -0,0 +1,24 @@
+import * as THREE from "three"
+import Sizes from "./Utils/Sizes.js"
+
+import Camera from "./Camera.js"
+import Renderer from "./Renderer.js"
+import Time from "./Utils/Time.js"
+
+
+
+export default class Experience {
+    static instance
+    constructor(canvas) {
+        if(Experience.instance) {
+            return Experience.instance
+        }
+        Experience.instance = this
+        this.canvas = canvas;
+        this.scene = new THREE.Scene();
+        this.time = new Time()
+        this.sizes = new Sizes();
+        this.camera = new Camera();
+        this.renderer = new Renderer();
+    }
+}
